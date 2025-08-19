1ed5dc782a1c46ea84fcc8133ddb51741eb86498
Audey Godfrey - Prewett <audeygodfrey@gmail.com>
2025-08-19 18:24:47 +0100

    30 done

---
+++ 30.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> out;
+
+        int wordLen = words[0].size();
+        int numWords = words.size();
+        int totalLen = wordLen * numWords;
+
+        unordered_map<string, int> wordCount;
+        for (auto &w : words) wordCount[w]++;
+
+        for (int i = 0; i <= s.size() - totalLen; i++) {
+            unordered_map<string, int> seen;
+            int j = 0;
+            for (; j < totalLen; j += wordLen) {
+                string word = s.substr(i + j, wordLen);
+                if (wordCount.find(word) == wordCount.end()) break;
+                seen[word]++;
+                if (seen[word] > wordCount[word]) break;
+            }
+            if (j == totalLen) out.push_back(i);
+        }
+
+        return out;
+    }
+};
