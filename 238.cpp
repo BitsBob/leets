82a7662ee5bd2061e7a3d7e8730df6d0e39d0f80
Audey Godfrey - Prewett <audeygodfrey@gmail.com>
2025-08-25 22:29:24 +0100

    238 done

---
+++ 238.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> out(nums.size(), 1);
+        int pre = 1;
+        for (int i=0; i<nums.size(); ++i) {
+            out[i] *= pre;
+            pre *= nums[i];
+        }
+
+        int post = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            out[i] *= post;
+            post *= nums[i];
+        }
+        
+        return out;
+    }
+};
