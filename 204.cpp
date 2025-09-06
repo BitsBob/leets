1d18ae1adfb2f8d03f26654f4e8e7dd0ee3877e3
Audey Godfrey - Prewett <audeygodfrey@gmail.com>
2025-09-06 17:43:15 +0100

    204 done

---
+++ 204.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <type_traits>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        vector<bool> sample(n, true);
+
+        sample[0] = sample[1] = false;
+
+        for (int i=2; i<sqrt(n); ++i) {
+            if (sample[i]) {
+                for (int j=i*i; j<n; j+=i) {
+                    sample[j] = false;
+                }
+            }
+        }
+
+        return std::count(sample.begin(), sample.end(), true);
+    }
+                
+};
